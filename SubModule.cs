f0b575e2225bd7f3812f5aedc334c18db8c50d45
gnivler <gnivil@gmail.com>
2022-11-01 09:28:13 -0600

    Initial commit

---
+++ SubModule.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using SandBox;
+using TaleWorlds.ModuleManager;
+using TaleWorlds.MountAndBlade;
+
+namespace SettlementDistanceCache
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            new Harmony("mod.bannerlord.SettlementDistanceCache").PatchAll();
+            base.OnSubModuleLoad();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+        }
+        
+        [HarmonyPatch(typeof(SettlementPositionScript), "SettlementsDistanceCacheFilePath", MethodType.Getter)]
+        public static class SettlementPositionScriptSettlementsDistanceCacheFilePath
+        {
+            public static void Postfix(ref string __result) => __result = ModuleHelper.GetModuleFullPath("RF-MAP") + "/ModuleData/settlements_distance_cache.bin";
+        }
+    }
+}
